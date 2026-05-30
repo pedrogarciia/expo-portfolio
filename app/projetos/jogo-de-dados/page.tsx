fd3683f4c25c09a3c81bd28b9aa2c232db274110
Pedro Garcia <108310232+pedrogarciia@users.noreply.github.com>
2026-05-30 00:30:36 -0300

    Delete app directory

--- app/projetos/jogo-de-dados/page.tsx
+++
@@ -1,9 +0,0 @@
-import JogoDados from "@/app/components/JogoDados";
-
-export default function JogoDeDadosPage() {
-  return (
-    <main className="min-h-screen bg-[radial-gradient(circle_at_top,_#27272a,_#09090b_55%)]">
-      <JogoDados />
-    </main>
-  );
-}
